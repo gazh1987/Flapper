b3354261279a5b1ac2a12eaafc1b8cfb2a4348ba
Gary Healy <gary.healy2@student.dit.ie>
2017-09-27 22:49:36 +0100

    User Input

--- app.js
+++ app.js
@@ -6,14 +6,31 @@ app.controller('MainCtrl', [
 	function($scope)
 	{
 		$scope.test = "Hello World!";
+		
 		$scope.posts = [
-			{title: 'post 1', upvotes: 5},
+			/*{title: 'post 1', upvotes: 5},
 			{title: 'post 2', upvotes: 2},
 			{title: 'post 3', upvotes: 15},
 			{title: 'post 4', upvotes: 9},
-			{title: 'post 5', upvotes: 4}
+			{title: 'post 5', upvotes: 4}*/
 		];
+
+		$scope.addPost = function()
+		{
+			if($scope.title === '') { return; } // Preventing User from posting a blank title
+			$scope.posts.push({
+				title: $scope.title, 
+				link: $scope.link, 
+				upvotes:0
+			});
+			//Clear textboxes
+			$scope.title = '';
+			$scope.link = '';  
+		};
+
+		$scope.incrementUpvotes = function(post)
+		{
+			post.upvotes += 1;
+		}
 	}
 ]);
-
-//Test Comment
